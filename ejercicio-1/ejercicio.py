b25d524def7dab41d7dea1082ac9d2892f3686c7
Moraga <lautaromoraga12@gmail.com>
2026-08-20 12:37:25 -0300

    agrego un avanze chiquito de python

---
+++ ejercicio-1/ejercicio.py
@@ -0,0 +1,53 @@
+#print(f"Hola Mundo") #ejercicio 1
+
+#import sys
+
+#nombre = sys.argv[1]
+#nombre_capitalizado = nombre.capitalize()
+
+#print(f"Hola {nombre_capitalizado}")
+
+#costo_pasaje = float(input("Ingrese costo estimado del pasaje: $"))
+#costo_alojamiento = float(input("Ingrese el costo del alojamiento por noche: $"))
+#cantidad_noches = int(input("Ingrese la cantidad de noches"))
+#dinero_disponible = float(input("Ingrese el dinero disponible: $"))
+
+#costo_total = costo_pasaje + (costo_alojamiento * cantidad_noches)
+
+#dinero_suficiente = dinero_disponible >= costo_total
+
+#print("\n --- Resultado del viaje ---")
+#print(f"Costo del pasaje: ${costo_pasaje:.2f}")
+#print(f"Costo total del alojamiento: ${costo_alojamiento * cantidad_noches:.2f}")
+#print(f"Costo total del viaje: ${costo_total:.2f}")
+#print(f"Dinero disponible: ${dinero_disponible:.2f}")
+#print(f"¿El dinero es suficiente?:{dinero_suficiente}")
+
+#def convertir_a_celsius(fahrenheit):
+    return(fahrenheit - 32) * 5 / 9
+
+#def convertir_a_fahrenheit(celsius):
+#    return celsius * 9 / 5 + 32
+
+#temperatura = float(input("Ingrese la temperatura: "))
+#escala_original = input("Ingrese la escala original (Celsius/Fahrenheit): ").strip().lower()
+
+#if escala_original == "celsius":
+#    temperatura_convertida = convertir_a_fahrenheit(temperatura)
+
+#    print(
+#        f"{temperatura:.2f} °C equivalen a "
+#        f"{temperatura_convertida:.2f} °F"
+#    )
+
+#elif escala_original == "fahrenheit":
+#    temperatura_convertida = convertir_a_celsius(temperatura)
+
+#    print(
+#        f"{temperatura:.2f} °F equivalen a "
+#        f"{temperatura_convertida:.2f} °C"
+#    )
+
+#else:
+#    print("La escala ingresada no es valida")
+
